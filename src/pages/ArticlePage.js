02c4370fe1f6e309681bfd5375ab2b7858ac32b4
NGV <gowriviswanath3@gmail.com>
2024-02-25 14:54:24 +0530

    front end completed

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,6 +1,21 @@
+import { useParams } from "react-router-dom";
+import articles from "./article-content"; 
+import NotFoundPage from "./NotFoundPage";
+
 const ArticlePage = () => {
+    const params = useParams();
+    const { articleId } = params;
+    const article = articles.find(article => article.name === articleId);
+    if(!article){
+        return <NotFoundPage />
+    }
     return (
-        <h1>This is the article page!</h1>
+        <>
+        <h1>{article.title}</h1>
+        {article.content.map(paragraph =>(
+            <p key={paragraph}>{paragraph}</p>
+        ))}
+        </>
     );
 }
 
